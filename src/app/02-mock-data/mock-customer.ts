6a23ebf9261f4f34db97581ff6deaf7b8ae09402
ManuelSchmidt <manne0081@gmail.com>
2022-04-01 18:15:42 +0200

    mock data

---
+++ src/app/02-mock-data/mock-customer.ts
@@ -0,0 +1,7 @@
+import { Customer } from "../01-model/customer";
+
+export const Customers: Customer[] = [
+    { 'id': 0, 'firstName': 'Dagobert', lastName: 'Duck' },
+    { 'id': 0, 'firstName': 'Daniel', lastName: 'Düsentrieb' },
+    { 'id': 0, 'firstName': 'Gustav', lastName: 'Gans' },
+]; 
---
+++ src/app/home-child01/homeChild01.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+import { Customer } from '../01-model/customer';
+import { Customers } from '../02-mock-data/mock-customer';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class BookingService {
+
+    constructor() {
+    }
+
+}
